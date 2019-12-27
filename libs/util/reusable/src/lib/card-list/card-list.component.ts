fc490929385529f417c56c55145a879d56e33334
Sandeep Desu <sdesu@pkrmsezdc.com>
2019-12-27 13:54:00 +0530

    added card-list module

---
+++ libs/util/reusable/src/lib/card-list/card-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'book-store-card-list',
+  templateUrl: './card-list.component.html',
+  styleUrls: ['./card-list.component.scss']
+})
+export class CardListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- libs/util/reusable/src/lib/reusable.module.ts
+++ libs/util/reusable/src/lib/reusable.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule
+} from '@angular/material';
+
+import { CardListComponent } from './card-list/card-list.component';
 import { HeaderComponent } from './header/header.component';
-import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule } from '@angular/material';
 import { SideNavComponent } from './side-nav/side-nav.component';
+
 @NgModule({
   imports: [
     CommonModule,
-    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
     MatIconModule,
+    MatListModule,
     MatSidenavModule,
-    MatListModule
+    MatToolbarModule
   ],
-  declarations: [HeaderComponent, SideNavComponent],
-  exports: [HeaderComponent, SideNavComponent],
-  entryComponents: [HeaderComponent, SideNavComponent]
+  declarations: [CardListComponent, HeaderComponent, SideNavComponent],
+  exports: [CardListComponent, HeaderComponent, SideNavComponent],
+  entryComponents: [CardListComponent, HeaderComponent, SideNavComponent]
 })
 export class ReusableModule { }

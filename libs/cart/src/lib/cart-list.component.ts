8180d01d82400517338d0592db35b6f0e964aeb0
Sandeep Desu <sdesu@pkrmsezdc.com>
2020-01-03 18:05:00 +0530

    updated interfaces with cart items also

--- libs/cart/src/lib/cart-list.component.ts
+++ libs/cart/src/lib/cart-list.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { StoreFacade } from '@book-store/store-management';
+import { CartItems } from '@book-store/util/reusable';
 @Component({
   selector: 'cart-cart-list',
   templateUrl: './cart-list.component.html',
   styleUrls: ['./cart-list.component.scss']
 })
-export class CartListComponent implements OnInit {
-  list;
-  amout: number = 0;
+export class CartListComponent implements OnInit, OnDestroy {
+  list: CartItems[];
+  amount: number = 0;
+  cartList;
   constructor(private facade: StoreFacade) { }
 
   ngOnInit() {
-    this.facade.cartList$.subscribe((items) => {
-      this.list = items;
+    this.cartList = this.facade.cartList$.subscribe((items) => {
+      if (items) {
+        const keys = Object.keys(items);
+        this.list = keys.map(key => items[key]);
+        this.list.forEach((item) => {
+          this.amount = this.amount + (item.count * item.amount);
+        })
+        console.log(this.amount);
+      }
     });
   }
 
+  ngOnDestroy(){
+    this.cartList.unsubscribe();
+  }
 }
